c6b0d887bde5ade9c396e63aac344dc4ed37a2a7
Lokendra Soni <lokendrasoni10@gmail.com>
2023-12-09 22:23:13 +0530

    fix: build

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,34 +1,36 @@
-import { Home, People, RocketLaunch } from '@mui/icons-material';
-import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-import { useRouter } from 'next/router';
-import { SidebarDiv } from 'pages/dao/style';
-import { useState } from 'react';
+import { Home, People, RocketLaunch } from "@mui/icons-material";
+import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { SidebarDiv } from "styles/style";
 
 export default function Sidebar() {
-  const router = useRouter();
-  const [active] = useState(router.pathname);
-  const navigation = [
-    { key: "dashboard", title: "Dashboard", icon: Home, link: "/dao/dashboard" },
-    { key: "quicksend", title: "Quick Send", icon: RocketLaunch, link: "/dao/quick-send" },
-    { key: "contacts", title: "Contributors", icon: People, link: "/dao/contributors" },
-  ]
+    const router = useRouter();
+    const [active] = useState(router.pathname);
+    const navigation = [
+        { key: "dashboard", title: "Dashboard", icon: Home, link: "/dao/dashboard" },
+        { key: "quicksend", title: "Quick Send", icon: RocketLaunch, link: "/dao/quick-send" },
+        { key: "contacts", title: "Contributors", icon: People, link: "/dao/contributors" },
+    ];
 
-
-  return (
-    <SidebarDiv>
-      <h2>Menu</h2>
-      <List>
-        {navigation.map((item) => (
-          <ListItem key={item.key} style={{ paddingLeft: 0, paddingRight: 0 }}>
-            <ListItemButton selected={active === item.link} onClick={() => router.push(item.link)}>
-              <ListItemIcon style={{ minWidth: '30px', color: "white" }}>
-                <item.icon />
-              </ListItemIcon>
-              <ListItemText primary={item.title} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </SidebarDiv>
-  );
+    return (
+        <SidebarDiv>
+            <h2>Menu</h2>
+            <List>
+                {navigation.map(item => (
+                    <ListItem key={item.key} style={{ paddingLeft: 0, paddingRight: 0 }}>
+                        <ListItemButton
+                            selected={active === item.link}
+                            onClick={() => router.push(item.link)}
+                        >
+                            <ListItemIcon style={{ minWidth: "30px", color: "white" }}>
+                                <item.icon />
+                            </ListItemIcon>
+                            <ListItemText primary={item.title} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </SidebarDiv>
+    );
 }

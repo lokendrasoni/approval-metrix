d6dd16ff7a3a95713b92fa1764c01f454582a545
Ashish Chawla <ashishmchawla@gmail.com>
2023-12-09 17:03:13 +0530

    Adding Contributor Table

--- pages/dao/contributors.tsx
+++ pages/dao/contributors.tsx
@@ -1,10 +1,11 @@
 import { Close } from '@mui/icons-material';
-import { Button, Checkbox, Dialog, DialogActions, DialogContent, DialogTitle, FormControlLabel, Grid, IconButton } from '@mui/material';
+import { Button, Checkbox, Dialog, DialogActions, DialogContent, DialogTitle, FormControlLabel, Grid, IconButton, Table } from '@mui/material';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import GoogleAuth from 'src/components/GoogleAuth';
 import Sidebar from 'src/components/Sidebar';
+import SafeContext from 'src/contexts/SafeContext';
 import { MainContent, StyledContainer } from "./style";
 
 export default function Contributors() {
@@ -13,6 +14,26 @@ export default function Contributors() {
   const [googleContacts, setGoogleContacts] = useState([]);
   const [showGoogleModal, setShowGoogleModal] = useState(false);
   const [selectedContacts, setSelectedContacts] = useState([]);
+
+  const { safeAddress, safeAuthSignInResponse } = useContext(SafeContext);
+
+  const rowsData: any = fetch('/api/safe/get_contributors', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'x-par-safe-address': safeAddress,
+      'x-par-network-id': '5',
+      'x-par-wallet-address': safeAuthSignInResponse?.eoa
+    },
+  }).then(obj => obj.json());
+
+  const data = rowsData?.data?.map((row) => {
+    return {
+      name: row.name,
+      email: row.email,
+      walletAddress: row.walletAddress
+    }
+  });
 
   async function getPeople() {
     const options = {
@@ -40,6 +61,36 @@ export default function Contributors() {
     console.log(selectedContacts);
   }, [selectedContacts]);
 
+  const columns = [
+    {
+      id: "name",
+      field: "name",
+      label: "Name",
+      styles: { width: "5%", borderRight: "1px solid rgba(224, 224, 224, 1)" },
+      renderCell: (data: any) => {
+        <p>{data?.name}</p>
+      }
+    },
+    {
+      id: "email",
+      field: "email",
+      label: "Email",
+      styles: { width: "5%", borderRight: "1px solid rgba(224, 224, 224, 1)" },
+      renderCell: (data: any) => {
+        <p>{data?.email}</p>
+      }
+    },
+    {
+      id: "address",
+      field: "walletAddress",
+      label: "Address",
+      styles: { width: "5%", borderRight: "1px solid rgba(224, 224, 224, 1)" },
+      renderCell: (data: any) => {
+        <p>{data?.walletAddress ? data?.walletAddress : "N/A"}</p>
+      }
+    },
+  ];
+
   const addContacts = async () => { };
 
   return (
@@ -51,12 +102,19 @@ export default function Contributors() {
       <MainContent>
         <Grid container>
           <Grid item xs={8}>
-            <h1 style={{ marginTop: '0' }}>Contributors</h1>
+            <h1 style={{ marginTop: '0', marginBottom: '10px' }}>Contributors</h1>
+            <p style={{ marginTop: '0' }}>Add contributors to your DAO by importing them from your Google Contacts.</p>
           </Grid>
           <Grid item xs={4}>
             <GoogleAuth />
           </Grid>
         </Grid>
+        <hr style={{ marginTop: '0', marginBottom: '20px' }} />
+
+        <Table
+          columns={columns}
+          rows={data}
+        ></Table>
 
 
         <Dialog sx={{

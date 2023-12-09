839316fedb21b79a3c85820ed3bde63dd8e98fd8
shivam5017 <shivammalik962@gmail.com>
2023-12-09 15:10:24 +0530

    feat/ui-added

--- src/modules/Contributor/Login/index.tsx
+++ src/modules/Contributor/Login/index.tsx
@@ -3,6 +3,12 @@ import { useContext } from "react";
 import ContributorContext from "src/contexts/ContributorContext";
 import { ContributorContextTypes } from "src/contexts/types/ContributorContextTypes";
 
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
 export default function ContributorLogin() {
     const { handleLogin } = useContext(ContributorContext) as ContributorContextTypes;
 
@@ -14,9 +20,36 @@ export default function ContributorLogin() {
             height={"100vh"}
             width={"100%"}
         >
-            <Button variant="contained" color="primary" onClick={handleLogin}>
-                Sign In with Contributor
-            </Button>
+            <Card sx={{ width: "580px", height: "580px" }}>
+                <CardMedia
+                    sx={{ height: 390 }}
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    title="green iguana"
+                />
+                <CardContent
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        marginBottom: "30px",
+                    }}
+                >
+                    <Typography gutterBottom variant="h5" component="div">
+                        Buildoors...
+                    </Typography>
+                </CardContent>
+                <CardActions
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                >
+                    <Button variant="contained" color="primary" onClick={handleLogin}>
+                        Sign In with Contributor
+                    </Button>
+                </CardActions>
+            </Card>
         </Box>
     );
 }
